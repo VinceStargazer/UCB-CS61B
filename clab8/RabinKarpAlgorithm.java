915cf7c020e1d936f8688eb53ad64970406e371d
guoqingyu <yujw88746113@gmail.com>
2022-02-20 23:24:18 -0400

    clab8 completed

--- clab8/RabinKarpAlgorithm.java
+++ clab8/RabinKarpAlgorithm.java
@@ -6,6 +6,23 @@ public class RabinKarpAlgorithm {
      * This method will return -1 if no matching substring is found, or if the input is invalid.
      */
     public static int rabinKarp(String input, String pattern) {
+        int length = pattern.length();
+        if (length > input.length()) {
+            return -1;
+        }
+        RollingString patternRoll = new RollingString(pattern, length);
+        RollingString inputRoll = new RollingString(input.substring(0, length), length);
+
+        if (patternRoll.equals(inputRoll)) {
+            return 0;
+        }
+
+        for (int i = length; i < input.length(); i++) {
+            inputRoll.addChar(input.charAt(i));
+            if (patternRoll.equals(inputRoll)) {
+                return i + 1 - length;
+            }
+        }
         return -1;
     }
 
