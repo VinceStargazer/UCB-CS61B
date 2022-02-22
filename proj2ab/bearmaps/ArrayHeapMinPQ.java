d0643fc2a2932718693ff45ee8a60e75a0b3df28
guoqingyu <yujw88746113@gmail.com>
2022-02-22 11:33:49 -0400

    ArrayHeapMinPQ implemented

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,176 @@
+package bearmaps;
+
+import java.util.*;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    private final ArrayList<PriorityNode> items;
+    private int size;
+    private final HashMap<T, Integer> dict;
+
+    private class PriorityNode implements Comparable<PriorityNode> {
+        private final T item;
+        private final double priority;
+
+        PriorityNode() {
+            this(null, Integer.MIN_VALUE);
+        }
+
+        PriorityNode(T item, double priority) {
+            this.item = item;
+            this.priority = priority;
+        }
+
+        T getItem() {
+            return item;
+        }
+
+        double getPriority() {
+            return priority;
+        }
+
+        @Override
+        public int compareTo(PriorityNode other) {
+            if (other == null) {
+                return -1;
+            }
+            return Double.compare(this.getPriority(), other.getPriority());
+        }
+    }
+
+    public ArrayHeapMinPQ() {
+        this(1);
+    }
+
+    public ArrayHeapMinPQ(int initCapacity) {
+        items = new ArrayList<>(initCapacity);
+        dict = new HashMap<>();
+        items.add(new PriorityNode());
+    }
+
+    @Override
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException("Item already exists");
+        }
+        items.add(new PriorityNode(item, priority));
+        dict.put(item, ++size);
+        swim(size);
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return dict.containsKey(item);
+    }
+
+    @Override
+    public T getSmallest() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Priority queue underflow");
+        }
+        return items.get(1).getItem();
+    }
+
+    @Override
+    public T removeSmallest() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Priority queue underflow");
+        }
+        T min = items.get(1).getItem();
+        swap(1, size--);
+        sink(1);
+        items.set(size + 1, null);
+        dict.remove(min);
+        return min;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void changePriority(T item, double priority) {
+        if (!contains(item)) {
+            throw new NoSuchElementException("No such item exists");
+        }
+        int k = dict.get(item);
+        items.set(k, new PriorityNode(item, priority));
+        swim(k);
+        sink(k);
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int leftChild(int k) {
+        return k * 2;
+    }
+
+    private int rightChild(int k) {
+        return k * 2 + 1;
+    }
+
+    private int parent(int k) {
+        return k / 2;
+    }
+
+    /** To compare between priorities of two items */
+    private boolean greater(int i, int j) {
+        return items.get(i).compareTo(items.get(j)) > 0;
+    }
+
+    /** To send eligible items upward */
+    private void swim(int k) {
+        if (parent(k) > 0 && greater(parent(k), k)) {
+            swap(k, parent(k));
+            swim(parent(k));
+        }
+    }
+
+    /** To send eligible items downward (if there's a tie, send left-down) */
+    private void sink(int k) {
+        while (leftChild(k) <= size) {
+            int smallerChild = leftChild(k);
+            if (rightChild(k) <= size && greater(leftChild(k), rightChild(k))) {
+                smallerChild = rightChild(k);
+            }
+            if (!greater(k, smallerChild)) break;
+            swap(k, smallerChild);
+            k = smallerChild;
+        }
+    }
+
+    /** To swap two items */
+    private void swap(int i, int j) {
+        T item = items.get(i).getItem();
+        T item2 = items.get(j).getItem();
+        dict.replace(item, j);
+        dict.replace(item2, i);
+        PriorityNode swap = items.get(i);
+        items.set(i, items.get(j));
+        items.set(j, swap);
+    }
+
+    /** Return the array format of priority queue */
+    private T[] toArray() {
+        T[] array = (T[]) new Object[size + 1];
+        array[0] = null;
+        for (int i = 1; i <= size; i++) {
+            array[i] = items.get(i).getItem();
+        }
+        return array;
+    }
+
+    /** Visualize some test output */
+    public static void main(String[] args) {
+        ArrayHeapMinPQ<Character> test = new ArrayHeapMinPQ<>();
+        String input = "abcdefg";
+        for (int i = 0; i < input.length(); i++) {
+            test.add(input.charAt(i), input.length() - i);
+        }
+        PrintHeapDemo.printFancyHeapDrawing(test.toArray());
+        test.removeSmallest();
+        PrintHeapDemo.printFancyHeapDrawing(test.toArray());
+    }
+}
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,148 @@
 package bearmaps;
 
+import edu.princeton.cs.algs4.Stopwatch;
 import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ArrayHeapMinPQTest {
+
+    @Test
+    public void sanityTest() {
+        ArrayHeapMinPQ<Character> test = new ArrayHeapMinPQ<>();
+        String input = "abcdefg";
+        for (int i = 0; i < input.length(); i++) {
+            test.add(input.charAt(i), input.length() - i);
+        }
+        assertEquals('g', (char) test.removeSmallest());
+        assertEquals('f', (char) test.getSmallest());
+        test.changePriority('a', 0);
+        assertEquals('a', (char) test.getSmallest());
+        assertTrue(test.contains('f'));
+        assertEquals(6, test.size());
+    }
+
+    @Test
+    public void edgeTest() {
+        ArrayHeapMinPQ<Character> test = new ArrayHeapMinPQ<>();
+        boolean[] thrown = new boolean[]{false, false, false, false};
+
+        try {
+            test.getSmallest();
+        } catch (NoSuchElementException e) {
+            thrown[0] = true;
+        }
+
+        try {
+            test.removeSmallest();
+        } catch (NoSuchElementException e) {
+            thrown[1] = true;
+        }
+
+        test.add('h', 12);
+        try {
+            test.add('h', 13);
+        } catch (IllegalArgumentException e) {
+            thrown[2] = true;
+        }
+
+        try {
+            test.changePriority('g', 12);
+        } catch (NoSuchElementException e) {
+            thrown[3] = true;
+        }
+
+        for (boolean b : thrown) {
+            assertTrue(b);
+        }
+    }
+
+    /** Test running time */
+    public static void main(String[] args) {
+        ArrayHeapMinPQ<Integer> test = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<Integer> test2 = new NaiveMinPQ<>();
+
+        // add speed test
+        Stopwatch sw = new Stopwatch();
+        for (int i = 0; i < 1_000_000; i++) {
+            test.add(i, 1_000_000 - i);
+        }
+        System.out.println("ArrayHeapMinPQ adds 1 mil items: " +
+                sw.elapsedTime() +  " seconds.");
+
+        sw = new Stopwatch();
+        for (int i = 0; i < 1_000_000; i++) {
+            test2.add(i, 1_000_000 - i);
+        }
+        System.out.println("NaiveMinPQ adds 1 mil items:  " +
+                sw.elapsedTime() +  " seconds.");
+        System.out.println();
+
+        // getSmallest speed test
+        sw = new Stopwatch();
+        for (int i = 0; i < 1000; i++) {
+            test.getSmallest();
+        }
+        System.out.println("ArrayHeapMinPQ runs getSmallest 1k times: " +
+                sw.elapsedTime() +  " seconds.");
+
+        sw = new Stopwatch();
+        for (int i = 0; i < 1000; i++) {
+            test2.getSmallest();
+        }
+        System.out.println("NaiveMinPQ runs getSmallest 1k times: " +
+                sw.elapsedTime() +  " seconds.");
+        System.out.println();
+
+        // removeSmallest speed test
+        sw = new Stopwatch();
+        for (int i = 0; i < 1000; i++) {
+            test.removeSmallest();
+        }
+        System.out.println("ArrayHeapMinPQ runs removeSmallest 1k times: " +
+                sw.elapsedTime() +  " seconds.");
+
+        sw = new Stopwatch();
+        for (int i = 0; i < 1000; i++) {
+            test2.removeSmallest();
+        }
+        System.out.println("NaiveMinPQ runs removeSmallest 1k times: " +
+                sw.elapsedTime() +  " seconds.");
+        System.out.println();
+
+        // contains speed test
+        sw = new Stopwatch();
+        for (int i = 0; i < 1000; i++) {
+            test.contains(i);
+        }
+        System.out.println("ArrayHeapMinPQ runs contains 1k times: " +
+                sw.elapsedTime() +  " seconds.");
+
+        sw = new Stopwatch();
+        for (int i = 0; i < 1000; i++) {
+            test2.contains(i);
+        }
+        System.out.println("NaiveMinPQ runs contains 1k times: " +
+                sw.elapsedTime() +  " seconds.");
+        System.out.println();
+
+        // changePriority speed test
+        sw = new Stopwatch();
+        for (int i = 0; i < 1000; i++) {
+            test.changePriority(i, i);
+        }
+        System.out.println("ArrayHeapMinPQ runs changePriority 1k times: " +
+                sw.elapsedTime() +  " seconds.");
+
+        sw = new Stopwatch();
+        for (int i = 0; i < 1000; i++) {
+            test2.changePriority(i, i);
+        }
+        System.out.println("NaiveMinPQ runs changePriority 1k times: " +
+                sw.elapsedTime() +  " seconds.");
+        System.out.println();
+    }
 }
